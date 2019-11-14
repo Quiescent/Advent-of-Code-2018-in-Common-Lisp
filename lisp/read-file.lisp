9a0e6a308055a1789984c11a1fb6fb6e0606a310
Edward John Steere <edward.steere@gmail.com>
2019-11-14 06:53:23 +0200

    Add a utility function to get the numbers from lines in a file

--- lisp/read-file.lisp
+++ lisp/read-file.lisp
@@ -1,3 +1,6 @@
+(ql:quickload "str")
+(ql:quickload "cl-ppcre")
+
 (defun file-lines (file-name)
   "Read the lines from FILE-NAME into a list."
   (with-open-file (in file-name)
@@ -5,3 +8,9 @@
       for line = (read-line in nil nil)
       while line
       collect line)))
+
+(defun file-lines-numbers (file-name)
+  "Read the lines from FILE-NAME into a list and extract all numbers.
+
+Numbers are separated by anything other than a number."
+  (str:words (ppcre:regex-replace "[^0-9]" (file-lines file-name) " ")))
